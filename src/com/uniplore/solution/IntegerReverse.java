0c08e44021222609509694fa0048a0286ca65fcf
zhujunpengguizhou <gzuzhujunpeng@gmail.com>
2017-05-24 15:46:06 +0800

    LeetCode第七题：反转整数

---
+++ src/com/uniplore/solution/IntegerReverse.java
@@ -0,0 +1,37 @@
+package com.uniplore.solution;
+
+public class IntegerReverse {
+
+	public int reverse(int x) {
+        int absX = 0;
+        if(x < 0){
+            absX = Math.abs(x); //将其转换成无符号整数
+        }else {
+            absX = x;
+        }
+        
+        int result = 0 ; 
+        char[] xChar = String.valueOf(absX).toCharArray();  //转换成char类型数组
+        for (int i = 0 ; i < (xChar.length)/2 ; i++) {
+            char temp = 0 ;
+            temp = xChar[i];
+            xChar[i] = xChar[xChar.length-1-i];
+            xChar[xChar.length-1-i] = temp;
+            //将字符数组转换成整型
+        }
+        try{
+            result = Integer.parseInt(String.valueOf(xChar));
+        }catch (Exception e){
+            return 0;
+        }
+        
+        
+        if (x < 0 ){
+            x = -1 * result;
+            return x;
+        }else {
+            x = result;
+            return x;
+        }
+    }
+}
