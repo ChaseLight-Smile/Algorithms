369fea32194c259bd2b0b0635c4ec54d2fc751c5
zhujunpengguizhou <gzuzhujunpeng@gmail.com>
2017-05-27 14:33:08 +0800

    修改判断细节

--- src/com/uniplore/solution/MaxSubArray.java
+++ src/com/uniplore/solution/MaxSubArray.java
@@ -57,11 +57,11 @@ public class MaxSubArray {
 			
 			
 			//对返回的三个结果进行比较
-			if (left[2] > right[2] && left[2] > crossing[2]) {  //如果最大子数组在左侧部分找到
+			if (left[2] >= right[2] && left[2] >= crossing[2]) {  //如果最大子数组在左侧部分找到
 				result = new int[3];
 				result = left;
 				return result;
-			}else if(right[2] > left[2] && right[2] > crossing[2]){ //如果最大子数组在右侧部分找到
+			}else if(right[2] >= left[2] && right[2] >= crossing[2]){ //如果最大子数组在右侧部分找到
 				result = new int[3];
 				result = right;
 				return right;
