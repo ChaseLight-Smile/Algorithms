0a9601a6855bc2684a0716374a8b8ae25b49454b
zhujunpengguizhou <gzuzhujunpeng@gmail.com>
2017-06-03 17:09:41 +0800

    随机算法测试

--- src/com/uniplore/solution/LeetCode204.java
+++ src/com/uniplore/solution/LeetCode204.java
@@ -11,11 +11,33 @@ public class LeetCode204 {
 
 	//该方法没有通过LeetCode测试，20个测试，最后的两个超时没有通过
 	public int countPrimes(int n) {
-		int count = 0 ;
+		/*int count = 0 ;
 		if(isPrime(n)){
 			count++;
 		}
-		return count;
+		return count;*/
+		 //素性测试的蒙特克罗方法，这个方法产生的结果太随机，对于很小的数都可能出现计算错误，尽管时间真的很少
+        //该算法是随机算法，不能直接用在这里想，需要采用随机算法思想进行改进
+        int count = 0 ;  //计数素数
+        for(int i = 2 ; i < n ; i++){
+            boolean flag = true;
+            int a = 0; 
+            while(flag){
+                a = (int)(java.lang.Math.random()*i);  //随机选择一个小于n的正整数，这里极其有可能会取到0
+                if(a == 0){
+                    flag = true;
+                }else{
+                    flag = false;
+                }
+            }
+            
+            if((int)((java.lang.Math.pow(a,i-1)-1) % i) == 0){
+                count++;
+            }else{
+                continue;
+            }
+        }
+        return count;
 	}
 	
 	public boolean isPrime(int n){
