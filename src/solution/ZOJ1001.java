d681771fe1caf985771e93f8ac349cd1d98167a6
zhujunpengguizhou <gzuzhujunpeng@gmail.com>
2017-06-05 15:31:36 +0800

    修改

--- src/solution/ZOJ1001.java
+++ src/solution/ZOJ1001.java
@@ -33,11 +33,17 @@ public class ZOJ1001 {
 			while((content = bufferedReader.readLine()) != null){
 				//整体读取到一行，并保存为String，因为拿空格分割，所以将两个操作数分开采用split函数
 				String[] ops = content.split(" ");
-				//ops数组此时会有两个元素，并且这两个元素将会被作为操作数，但是是否一定是合法的？应该将String转成int
-			    int ops1 = Integer.valueOf(ops[0]);   //将String类型转换成Integer，用到了JDK1.5之后的自动拆箱特性将Integer转成int
-			    //int ops1 = Integer.parseInt(ops[0]);   //同样可以采用parseInt(String s)函数将字符串转换成整型int
-			    int ops2 = Integer.valueOf(ops[1]);
-			    System.out.println(ops1+ops2);
+				if(ops.length < 2 || ops.length > 2){
+					System.out.println("操作数数目不正确请重新输入，操作数个数必须为2，并且用空格分割");
+					continue;
+				}else {
+					//ops数组此时会有两个元素，并且这两个元素将会被作为操作数，但是是否一定是合法的？应该将String转成int
+				    int ops1 = Integer.valueOf(ops[0]);   //将String类型转换成Integer，用到了JDK1.5之后的自动拆箱特性将Integer转成int
+				    //int ops1 = Integer.parseInt(ops[0]);   //同样可以采用parseInt(String s)函数将字符串转换成整型int
+				    int ops2 = Integer.valueOf(ops[1]);
+				    System.out.println(ops1+ops2);
+				}
+				
 			}
 		} catch (IOException e) {
 			throw new RuntimeException();
