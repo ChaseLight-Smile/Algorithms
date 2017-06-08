40e9a794535b1f1c6a384c75ff17a39958f8e594
zhujunpengguizhou <gzuzhujunpeng@gmail.com>
2017-06-08 15:11:38 +0800

    修改

--- src/solution/ZOJ1001.java
+++ src/solution/ZOJ1001.java
@@ -239,14 +239,4 @@ public class ZOJ1001 {
 			throw new RuntimeException();
 		}	
 	}
-	
-	/** hdu1003
-	 * Problem: Given a sequence a[1],a[2],a[3]......a[n], your job is to calculate the max sum of a sub-sequence. For example, given (6,-1,5,4,-7), the max sum in this sequence is 6 + (-1) + 5 + 4 = 14.
-	 * Input: The first line of the input contains an integer T(1<=T<=20) which means the number of test cases. Then T lines follow, each line starts with a number N(1<=N<=100000), then N integers followed(all the integers are between -1000 and 1000).
-	 * Output: For each test case, you should output two lines. The first line is "Case #:", # means the number of the test case. The second line contains three integers, the Max Sum in the sequence, the start position of the sub-sequence, the end position of the sub-sequence. If there are more than one result, output the first one. Output a blank line between two cases.
-	 * 本质最大子数组问题，代码之前已经写过，参见MaxSubArray.java文件
-	 */
-	public static void HDU1003Solution(){
-		//参见MaxSubArray.java文件
-	}
 }
