f0f1cf9c3d937bef73445fd741c24e6d524b55c5
JunpengCode <jpzhu.gm@gmail.com>
2018-04-09 20:08:43 +0800

    modify the quicksort

--- quicksort.cpp
+++ quicksort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 /**
-  * Hoare partition. It is the first method that was introduced by Hoare.
+  * Hoare partition.It is the first method that was introduced by Hoare.
   * It is also the first method that was used in the quicksort algorithm.
   * @arr store the unsorted elements
   * @start the start position
@@ -14,7 +14,7 @@ int partitionHoare(int arr[], int start, int end){
     int cursorRight = end + 1;   //set cursor for the right value
     int key = arr[start];   // set key value as standard
     while(true){
-        do{
+        do{   //It should be avoided to use do...while
             cursorLeft++;
         }while(arr[cursorLeft]<key);
         do{
@@ -32,6 +32,35 @@ int partitionHoare(int arr[], int start, int end){
 }
 
 /**
+  * Hoare partition modify. It is the first method that was introduced by Hoare.
+  * It is also the first method that was used in the quicksort algorithm.
+  * @arr store the unsorted elements
+  * @start the start position
+  * @end the end position
+  * @author Junpeng Zhu
+*/
+int partitionHoareModify(int arr[], int start, int end){
+    int cursorLeft = start; //set cursor for the left value
+    int cursorRight = end;   //set cursor for the right value
+    int key = arr[start];   // set key value as standard
+    while(true){
+        while(arr[cursorLeft]<key){
+            cursorLeft++;
+        }
+        while(arr[cursorRight]>key){
+            cursorRight--;
+        }
+        if(cursorLeft < cursorRight){
+            //swap the values
+            int temp = arr[cursorLeft];
+            arr[cursorLeft] = arr[cursorRight];
+            arr[cursorRight] = temp;
+        }else{
+            return cursorRight;
+        }
+    }
+}
+/**
   * quicksort algorithm. It is the first introduced by Hoare.
   * @arr store the unsorted elements
   * @start the start position
@@ -40,7 +69,7 @@ int partitionHoare(int arr[], int start, int end){
 */
 void quicksort(int arr[],int start, int end){
     if(start < end){
-        int position = partitionHoare(arr,start,end);
+        int position = partitionHoareModify(arr,start,end);  //use partitionHoare is error because of do...while
         quicksort(arr,start,position-1);
         quicksort(arr,position+1,end);
     }
@@ -49,15 +78,14 @@ void quicksort(int arr[],int start, int end){
 int main(int args, char* argv[])
 {
     //int arr[] = {3,2,1};  //success
-    //int arr[] = {2,8,7,1,3,5,6,4};   //error
+    int arr[] = {2,8,7,1,3,5,6,4};   //success
     //int arr[] = {3,5,4,6,2,1};  //success
     //int arr[] = {2,5,2,4,1,1};   //success
-    int arr[] = {13,19,9,5,12,8,7,4,11,2,6,21};   //error
+    //int arr[] = {13,19,9,5,12,8,7,4,11,2,6,21};   //success
     quicksort(arr,0,sizeof(arr)/sizeof(arr[0])-1);
     for(int i = 0 ; i < sizeof(arr)/sizeof(arr[0]);i++){
         cout << arr[i] << " ";
     }
     return 0;
 }
-
 
