a8ee75f7e414441d392ef54c67cf54cad7044c96
JunpengCode <jpzhu.gm@gmail.com>
2018-04-30 22:53:17 +0800

    Add: add HDUOJ2007 in page 11

--- Code/HDU2007.cpp
+++ Code/HDU2007.cpp
@@ -7,6 +7,11 @@ int main(){
         int sum1 = 0;
         int sum2= 0;
         cin >> b;
+        if (a>b){
+            int temp = a;
+            a = b;
+            b = temp;
+        }
         for (int i = a ; i <= b ; i++){
             if (i % 2 == 0){
                 sum1 += i*i;
@@ -14,7 +19,7 @@ int main(){
                 sum2 += i * i * i;
             }
         }
-        cout << sum1 << " " << sum2 << " "<< endl;
-    } 
+        cout << sum1 << " " << sum2 << endl;
+    }
     return 0;
 }
