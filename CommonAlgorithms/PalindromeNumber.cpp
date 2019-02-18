74062d5b39d1468175e35f3dec0df71c5272e4ca
JunpengCode <jpzhu.gm@gmail.com>
2019-02-18 10:29:34 +0800

    modify

--- CommonAlgorithms/PalindromeNumber.cpp
+++ CommonAlgorithms/PalindromeNumber.cpp
@@ -5,26 +5,28 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if (x < 0) {
-        return false;
-    }else{
-        int *p = new int[10];
-        int size = 0;
-        while (x != 0 && size <= 9){
-            p[size] = x % 10;
-            x /= 10;
-            size++;
-        }
-        size--;
-        for (int i = 0; i <= size/2; i++){
-            if (p[i] != p[size-i]){
-                delete[] p;
-                return false;
-            }else{
-                continue;
+            return false;
+        }else if (x == 0){
+            return true;
+        }else{
+            int *p = new int[10];
+            int size = 0;
+            while (x != 0 && size <= 9){
+                p[size] = x % 10;
+                x /= 10;
+                size++;
             }
+            size--;
+            for (int i = 0; i <= size/2; i++){
+                if (p[i] != p[size-i]){
+                    delete[] p;
+                    return false;
+                }else{
+                    continue;
+                }
+            }
+            delete[] p;
+         return true;
         }
-        delete[] p;
-        return true;
-    }
-    }
+        }
 };
