ee02c6dc92066c32559f2d8b859673f6050df732
JunpengCode <jpzhu.gm@gmail.com>
2020-04-03 15:41:49 +0800

    add leetcode 148 solution

--- CommonAlgorithms/leetcode148.cpp
+++ CommonAlgorithms/leetcode148.cpp
@@ -1,29 +1,31 @@
 /**
-* Sort a linked list in O(n log n) time using constant space complexity.
-
-* Example 1:
-*   Input: 4->2->1->3
-*   Output: 1->2->3->4
-
-* Example 2:
-*   Input: -1->5->3->4->0
-*   Output: -1->0->3->4->5
-*/
-
-
-#include <iostream>
-using namespace std;
-
-// Definition for singly-linked list.
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {}
-};
- 
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-              
+        vector<int> tmp;
+        ListNode* current = head;
+        while(current != NULL){
+            tmp.push_back(current->val);
+            current = current->next;
+        }
+        
+        sort(tmp.begin(), tmp.end());
+        
+        ListNode* ans = new ListNode(0);
+        current = ans;
+        for(int i = 0 ; i < tmp.size(); i++){
+            ListNode* a = new ListNode(tmp[i]);
+            current->next = a;
+            current = current->next;
+        }
+        
+        return ans->next;
     }
 };
