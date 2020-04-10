9d448a81aae0940bc421cf39dd0a629781d11acc
JunpengCode <jpzhu.gm@gmail.com>
2020-04-10 17:39:22 +0800

    modify leetcode 347 solution

--- CommonAlgorithms/LeetCode347.cpp
+++ CommonAlgorithms/LeetCode347.cpp
@@ -1,3 +1,4 @@
+//解法一 hash+heap
 struct queue_node{
     int key;
     int count;
@@ -37,5 +38,38 @@ public:
             k--;
         }
         return ans;
+    }
+};
+
+//解法二 双map
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int, int> m;
+        map<int,int>::iterator m_it;
+        int len = nums.size();
+        for(int i = 0 ; i < len; i++){
+            if(m.find(nums[i]) != m.end()){
+                m[nums[i]] = m[nums[i]]+1;
+            }else{
+                m[nums[i]] = 1;
+            }
+        }
+        
+        multimap<int, int> ans;
+        map<int, int>::reverse_iterator mm_it;
+        for(m_it = m.begin(); m_it!= m.end(); m_it++){
+            ans.insert(pair<int, int>(m_it->second, m_it->first));
+        }
+        
+        
+        vector<int> result;
+        for(mm_it = ans.rbegin(); mm_it!= ans.rend(), k>0; mm_it++){
+            result.push_back(mm_it->second);
+            k--;
+        }
+        
+        return result;
+        
     }
 };
