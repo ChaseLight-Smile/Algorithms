c92749a6f430cc1e8ead93c8570df398c465e30f
JunpengCode <jpzhu.gm@gmail.com>
2020-04-13 13:21:45 +0800

    modify leetcode 162

--- CommonAlgorithms/LeetCode162.cpp
+++ CommonAlgorithms/LeetCode162.cpp
@@ -19,3 +19,18 @@ public:
         return ans;
     }
 };
+
+//解法二： 实际上只需要比较单侧就可以，时间复杂度还是O(n)
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int len = nums.size();
+        for(int i = 1 ; i < len; i++){
+            if(nums[i] < nums[i-1]){
+                return i-1;
+            }
+        }
+        return len-1;
+    }
+};
+
