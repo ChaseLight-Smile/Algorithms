22fd4dacfb5572ec7c1a5369df8529ec7500671f
JunpengCode <jpzhu.gm@gmail.com>
2020-04-15 17:13:05 +0800

    modify

--- CommonAlgorithms/LeetCode981.cpp
+++ CommonAlgorithms/LeetCode981.cpp
@@ -90,6 +90,48 @@ public:
     }
 };
 
+
+//解法三： unordered_map
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
+ 
+ class TimeMap {
+public:
+    unordered_map<string, string> m;
+public:
+    /** Initialize your data structure here. */
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        string combinedKey = key+to_string(timestamp);
+        m.insert({combinedKey,value});
+    }
+    
+    string get(string key, int timestamp) {
+        if(m.find(key+to_string(timestamp)) != m.end()){
+            return m[key+to_string(timestamp)];
+        }else{
+            while(1){
+                timestamp--;
+                if(m.find(key+to_string(timestamp)) != m.end()){
+                    return m[key+to_string(timestamp)];
+                }
+                if(timestamp == 1){
+                    break;
+                }
+            }
+            return "";
+        }
+       
+    }
+};
+
 /**
  * Your TimeMap object will be instantiated and called as such:
  * TimeMap* obj = new TimeMap();
