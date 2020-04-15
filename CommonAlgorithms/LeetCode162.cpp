eddf36a99e5ea1b5bfb590db89920ce58332d92e
JunpengCode <jpzhu.gm@gmail.com>
2020-04-15 22:19:25 +0800

    modify leetcode 162 solution

--- CommonAlgorithms/LeetCode162.cpp
+++ CommonAlgorithms/LeetCode162.cpp
@@ -50,3 +50,28 @@ public:
         return right;
     }
 };
+
+//二分查找
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int len = nums.size();
+        if(len == 1){
+            return 0;
+        }
+        
+        int left = 0 ;
+        int right = len-1;
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(mid-1>=0 && nums[mid] < nums[mid-1]){
+                right = mid-1;
+            }else if(mid+1<len && nums[mid] < nums[mid+1]){
+                left = mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return left;
+    }
+};
