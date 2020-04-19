55eff470cb435b04441d48f3be18b8aa15ba1055
JunpengCode <jpzhu.gm@gmail.com>
2020-04-19 17:59:57 +0800

    modify leetcode 154 solution AC

--- CommonAlgorithms/LeetCode154.cpp
+++ CommonAlgorithms/LeetCode154.cpp
@@ -1,3 +1,4 @@
+//时空复杂度为O(n)
 class Solution {
 public:
     int findMin(vector<int>& nums) {
@@ -10,7 +11,7 @@ public:
     }
 };
 
-//二分解法，但是由于重复元素的存在，导致出现了破坏单调性的情况，应该特殊处理
+//二分解法，时间复杂度O(logn)，空间复杂度O(1)
 class Solution {
 public:
     int findMin(vector<int>& nums) {
@@ -24,15 +25,17 @@ public:
         while(left < right){
             int mid = left + (right-left)/2;
             if(nums[mid] < nums[right]){
-                //说明右边一定有有序
                 right = mid;
             }else if (nums[mid] > nums[right]){
                 left = mid+1;
             }else if(nums[mid] == nums[right]){
-                //如果相等，表明这个位置破坏了原来数据的单调性，应该特殊处理
-                right--;
+                //[3,3,5,3]
+                if(nums[right] == nums[right-1]){
+                    right--;
+                }else{
+                    left++;
+                }
             }
-                
         }
         return left;
     }
