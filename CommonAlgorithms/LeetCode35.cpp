d606e8a0ebc0b6bb46d1bbc1d4402dee0fc7a060
JunpengCode <jpzhu.gm@gmail.com>
2020-04-20 22:41:34 +0800

    update leetcode 35 solution

--- CommonAlgorithms/LeetCode35.cpp
+++ CommonAlgorithms/LeetCode35.cpp
@@ -1,36 +1,25 @@
-/**
- * success Runtime: 4 ms, faster than 97.78% of C++ online submissions for Search Insert Position.
- * Memory Usage: 6.9 MB, less than 100.00% of C++ online submissions for Search Insert Position.
-*/
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int len = nums.size();
-        vector<int>::iterator it = find(nums.begin(), nums.end(), target);
-        if(it != nums.end()){
-            return &*it - &nums[0];
-        }
-        if(target < nums[0]){
+        if(len == 0){
             return 0;
         }
-        if(target > nums[len-1]){
-            return len;
-        }
-        int mid = (len-1)/2;
-        while(1){
-            if(target > nums[mid]){
-                if (target < nums[mid+1])
-                    return mid+1;
-                else
-                    mid = (mid + 1 + len-1)/2;
+        int left = 0;
+        int right = len-1;
+        while(left < right){
+            int mid = left + (right-left+1)/2;
+            if(nums[mid] > target){
+                right = mid-1;
+            }else if(nums[mid] == target){
+                return mid;
+            }else{
+                left = mid;
             }
-            if(target < nums[mid]){
-                if(target > nums[mid-1]){
-                    return mid;
-                }else
-                    mid = (mid-1+0)/2;
-            }
-        
         }
+        if(nums[left] >= target){
+            return left;
+        }
+        return left+1;
     }
 };
