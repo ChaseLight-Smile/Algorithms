d23803587493b6bdce91b179bc2a88aca1e9db66
JunpengCode <jpzhu.gm@gmail.com>
2020-04-20 11:17:56 +0800

    add leetcode 215 solution AC

--- CommonAlgorithms/LeetCode215.cpp
+++ CommonAlgorithms/LeetCode215.cpp
@@ -36,3 +36,52 @@ public:
         return q.top();
     }
 };
+
+//binary search
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int len = nums.size();
+        if(len == 0){
+            return -1;
+        }
+        int left = 0 ;
+        int right = len-1;
+        swap(nums[left + (right-left)/2], nums[left]);
+        int mid  = find(nums,left,right);
+        while(true){
+            if(mid == len-k){
+                break;
+            }else if(mid > len-k){
+                mid = find(nums, left, mid-1);
+            }else{
+                mid = find(nums, mid+1, right);
+            }
+        }
+        return nums[mid];
+        
+    }
+    int find(vector<int>& nums,int start, int end){
+        int key = nums[start];
+        int left = start;
+        int right = end;
+        while(left < right){
+            while(left < right && nums[right] >= key){
+                right--;
+            }
+            if(left < right){
+                nums[left]=nums[right];
+                left++;
+            }
+            while(left < right && nums[left] < key){
+                left++;
+            }
+            if(left<right){
+                nums[right]= nums[left];
+                right--;
+            }
+        }
+        nums[left] = key;
+        return left;
+    }
+};
