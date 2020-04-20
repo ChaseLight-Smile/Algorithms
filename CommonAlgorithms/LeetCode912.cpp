af1095f478438ac7c43f54487ea7d4c7a7877296
JunpengCode <jpzhu.gm@gmail.com>
2020-04-20 13:53:34 +0800

    add heap sort

--- CommonAlgorithms/LeetCode912.cpp
+++ CommonAlgorithms/LeetCode912.cpp
@@ -1,3 +1,4 @@
+//快速排序
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
@@ -30,5 +31,24 @@ public:
             quicksort(nums, start, p-1);
             quicksort(nums, p+1, end);
         }
+    }
+};
+
+//堆排序
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        priority_queue<int, vector<int>, greater<int>>q;
+        int len = nums.size();
+        for(int i = 0; i < len; i++){
+            q.push(nums[i]);
+        }
+        nums.clear();
+        while(!q.empty()){
+            int tmp = q.top();
+            nums.push_back(tmp);
+            q.pop();
+        }
+        return nums;
     }
 };
