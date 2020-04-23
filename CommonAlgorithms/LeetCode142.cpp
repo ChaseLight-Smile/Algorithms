d4fe5b899976eb49a2568321ab09ba0cb5126429
JunpengCode <jpzhu.gm@gmail.com>
2020-04-23 18:40:50 +0800

    update leetcode 142 solution fast-slow pointer

--- CommonAlgorithms/LeetCode142.cpp
+++ CommonAlgorithms/LeetCode142.cpp
@@ -31,3 +31,32 @@ public:
 };
 
 //快慢指针
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow){
+                //相遇了
+                fast = head;
+                while(fast != slow){
+                    fast = fast->next;
+                    slow = slow->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
