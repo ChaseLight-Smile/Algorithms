7026e3f0896c4b8a00fd5574061305520d456a94
JunpengCode <jpzhu.gm@gmail.com>
2020-05-05 10:32:47 +0800

    update leetcode 74 solution

--- Traditional-Algorithms/LeetCode74.cpp
+++ Traditional-Algorithms/LeetCode74.cpp
@@ -35,3 +35,26 @@ public:
         return false;
     }
 };
+
+//线性单调性二分
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.size() == 0 || matrix[0].size() == 0){
+            return false;
+        }
+        int left = 0, right = matrix.size()* matrix[0].size()-1;
+        while(left < right){
+            int mid = left + right + 1 >> 1;
+            if(matrix[mid/matrix[0].size()][mid%matrix[0].size()] > target){
+                right = mid-1;
+            }else{
+                left = mid;
+            }
+        }
+        if(matrix[left/matrix[0].size()][left%matrix[0].size()] == target){
+            return true;
+        }
+        return false;
+    }
+};
