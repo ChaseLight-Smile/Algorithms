e6872827775424881db7f5fa836c95660f94b0a8
JunpengCode <jpzhu.gm@gmail.com>
2020-05-06 15:10:06 +0800

    update leetcode 17 solution using dfs+backtracking template

--- Traditional-Algorithms/LeetCode17.cpp
+++ Traditional-Algorithms/LeetCode17.cpp
@@ -84,3 +84,36 @@ public:
         return;
     }
 };
+
+
+//统一的DFS+backtracking模式
+class Solution {
+private:
+    string character[8] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    stack<string>st;  //保护现场
+    string s;   //一个解
+    vector<string> ans;  //全部解
+public:
+    vector<string> letterCombinations(string digits) {
+        if(digits.size() == 0){
+            return vector<string>();
+        }
+        dfs(digits, 0);
+        return ans;
+    }
+    
+    void dfs(string digits, int start){
+        if(start == digits.size()){
+            ans.push_back(s);
+            return;
+        }
+        for(int i = 0 ; i < character[digits[start]-'2'].size(); ++i){
+            st.push(s);  //保护现场
+            s = s + character[digits[start]-'2'][i];
+            dfs(digits, start+1);
+            s = st.top();   //恢复现场
+            st.pop();  //恢复现场
+        }
+        return;
+    }
+};
