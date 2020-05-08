bff290ac8b4e20de927a181c482388c9f528b922
JunpengCode <jpzhu.gm@gmail.com>
2020-05-08 14:22:51 +0800

    update leetcode 215 solution using quick selection algorithms AC

--- Traditional-Algorithms/LeetCode215.cpp
+++ Traditional-Algorithms/LeetCode215.cpp
@@ -85,3 +85,63 @@ public:
         return left;
     }
 };
+
+
+//使用快速选择算法
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+       return quicksort(nums, 0, nums.size()-1, k);
+    }
+    int quicksort(vector<int>& nums, int l, int r, int k){
+        if(l == r){
+            return nums[l];
+        }
+        int pivot = nums[l];
+        int i = l-1;
+        int j = r+1;
+        while(i < j){
+            do ++i; while(nums[i] > pivot);  //注意这里是从大到小排序
+            do --j; while(nums[j] < pivot);
+            if(i < j) swap(nums[i], nums[j]);
+        }
+        int sl = j - l + 1;
+        if(k <= sl){
+            return quicksort(nums, l, j, k);
+        }
+        return quicksort(nums, j+1, r, k-sl);
+    }
+};
+
+//求第k小数的算法，也是使用快速选择算法
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> nums;
+int quicksort(int left, int right, int k){
+    if(left == right) return nums[left];
+    int i = left-1;
+    int j = right+1;
+    int pivot = nums[left];
+    while(i < j){
+        do ++i; while(nums[i] < pivot);
+        do --j; while(nums[j] > pivot);
+        if(i < j) swap(nums[i],nums[j]);
+    }
+    int sl = j-left+1;
+    if(k <= sl){
+        return quicksort(left, j, k);
+    }
+    return quicksort(j+1, right, k-sl);
+}
+
+int main(){
+    int n , k;
+    cin >> n >> k;
+    nums = vector<int>(n);
+    for(int i = 0 ; i < n; ++i){
+        scanf("%d", &nums[i]);
+    }
+    cout << quicksort(0, n-1, k) << endl;
+    
+    return 0;
+}
