f90d62c7393ef867ce6183c3e15738189543de08
JunpengCode <jpzhu.gm@gmail.com>
2020-05-12 10:13:02 +0800

    add POJ 1089 solution using greedy AC

--- Traditional-Algorithms/LeetCode56.cpp
+++ Traditional-Algorithms/LeetCode56.cpp
@@ -32,3 +32,45 @@ public:
         return ans;
     }
 };
+
+
+//POJ1089  AC  greedy
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef pair<int, int> PII;
+vector<PII> range;
+
+void merge(vector<PII>& range){
+    vector<PII> ans;
+    sort(range.begin(), range.end());  //sort提供对pair进行排序，默认按照first排序，如果first相同按照second排序
+    
+    int start = -2e9, end = -2e9;
+    for(auto r : range){
+        if(r.first > end){
+            //说明两个区间没有交点
+            start = r.first;
+            end = r.second;
+            if(start != -2e9) ans.push_back({start, end});
+        }else{
+            end = max(end, r.second);
+        }
+    }
+    range = ans;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    for(int i = 0 ; i < n; i++){
+        int l , r;
+        scanf("%d%d", &l, &r);
+        range.push_back({l, r});
+    }
+    
+    merge(range);
+    printf("%d", range.size());
+    return 0;
+}
