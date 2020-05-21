3e44c339504396eee750451c35418b6e797ead85
JunpengCode <jpzhu.gm@gmail.com>
2020-05-21 09:28:22 +0800

    modify topology sort template

--- Traditional-Algorithms/topsort.cpp
+++ Traditional-Algorithms/topsort.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 const int N = 1e5+10;
-int h[N], e[N], ne[N], idx;
-int q[N], d[N]; //q表示队列,d表示节点的入度
-int n , m;
+int h[N], e[N], ne[N], idx; // 链式前向星
+int q[N], d[N]; //q为队列，d表示图中节点的入度集合
+int n,m;
 
 void add(int a , int b){
     e[idx] = b;
@@ -17,13 +17,12 @@ bool topsort(){
     int hh = 0, tt = -1;
     for(int i = 1; i <=n; i++){
         if(!d[i]){
-            q[++tt] = i;
+            q[++tt] = i;   //如果这里需要将输出的结果序列按照某种次序排序，可以该用priority_queue来存储
         }
     }
-    
     while(hh <= tt){
-        int t = q[hh++];
-        for(int i = h[t]; i != -1; i = ne[i]){
+        int u = q[hh++];
+        for(int i = h[u]; i != -1; i = ne[i]){
             int j = e[i];
             d[j]--;
             if(d[j] == 0){
@@ -34,22 +33,20 @@ bool topsort(){
     if(tt == n-1){
         return true;
     }else return false;
-    
 }
 
 int main(){
-    cin >> n >> m;
-    memset(h, -1, sizeof(h));
+    scanf("%d%d", &n, &m);
+    memset(h, -1, sizeof h);  //初始化链式前向星
     while(m--){
         int a, b;
-        cin >> a >> b;
-        add(a,b);
+        scanf("%d%d", &a, &b);
+        add(a, b);
         d[b]++;
     }
     if(topsort()){
         for(int i = 0; i < n; i++){
-            cout << q[i] << " "; 
+            printf("%d ", q[i]);
         }
-    }else puts("-1\n");
-    return 0;
+    }else printf("-1\n");
 }
