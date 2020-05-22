c4bfbb7fa15224a5792db3a6f13b0d98e3a479e1
JunpengCode <jpzhu.gm@gmail.com>
2020-05-22 15:25:06 +0800

    add dijkstra+ heap template

--- Traditional-Algorithms/Dijkstra.cpp
+++ Traditional-Algorithms/Dijkstra.cpp
@@ -41,3 +41,60 @@ int main(){
     printf("%d", k);
     return 0;
 }
+
+
+//堆优化dijkstra算法，时间复杂度为O(mlogn)，使用链式前向星存储
+#include <iostream>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+const int N = 150010;
+typedef pair<int, int>PII;
+int h[N], e[N], ne[N], w[N], idx;
+int dist[N];
+bool visited[N];
+int n, m;
+
+void add(int a, int b, int c){
+    e[idx] = b;
+    w[idx] = c;
+    ne[idx] = h[a];
+    h[a] = idx++;
+}
+
+int dijkstra(){
+    memset(dist, 0x3f, sizeof dist);
+    dist[1] = 0;
+    priority_queue<PII, vector<PII>, greater<PII>>q;
+    q.push({0, 1}); //0表示1到1的距离，1表示起始点
+    while(!q.empty()){
+        auto t = q.top();
+        q.pop();
+        int dis = t.first, point = t.second;
+        if(visited[point]) continue;
+        visited[point] = true;
+        for(int i = h[point]; i != -1; i = ne[i]){
+            int j = e[i];
+            if(dist[j] > dis + w[i]){
+                dist[j] = dis + w[i];
+                q.push({dist[j], j});
+            }
+        }
+    }
+    if(dist[n] == 0x3f3f3f3f) return -1;
+    return dist[n];
+}
+
+int main(){
+    scanf("%d%d", &n, &m);
+    memset(h, -1, sizeof h);
+    while(m--){
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+        add(a, b, c);
+    }
+    int k = dijkstra();
+    printf("%d", k);  
+    return 0;
+}
