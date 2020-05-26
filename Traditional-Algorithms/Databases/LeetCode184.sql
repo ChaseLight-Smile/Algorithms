a605dfc6f9523bea79c707b1a51378258b30df22
JunpengCode <jpzhu.gm@gmail.com>
2020-05-26 18:44:12 +0800

    modify leetcode 184 sql solution AC

--- Traditional-Algorithms/Databases/LeetCode184.sql
+++ Traditional-Algorithms/Databases/LeetCode184.sql
@@ -7,3 +7,18 @@ Department as D
 where E.salary = T.max and 
 E.DepartmentId = D.Id and
 E.DepartmentId = T.DepartmentId
+
+
+
+# 传统方法
+select D.Name as Department, E.Employee, E.Salary
+from Department as D,
+(select E1.Id, E1.Name as Employee, E1.DepartmentId, E1.Salary
+from Employee as E1,
+(select max(Salary) as Salary, DepartmentId
+from Employee
+group by DepartmentId) as E2
+where E1.DepartmentId = E2.DepartmentId  and E1.Salary = E2.Salary) as E
+where D.Id = E.DepartmentId
+order by Salary desc
+
