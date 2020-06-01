16b586589cf432b64871828494c7607e550a1476
JunpengCode <jpzhu.gm@gmail.com>
2020-06-01 10:29:13 +0800

    modify leetcode 889 solution using recursive AC

--- Traditional-Algorithms/LeetCode889.cpp
+++ Traditional-Algorithms/LeetCode889.cpp
@@ -16,7 +16,7 @@ public:
     TreeNode* buildTree(vector<int>& pre, int preLeft, int preRight, vector<int>& post, int postLeft, int postRight){
         if(preLeft > preRight || postLeft > postRight) return nullptr;
         TreeNode * root = new TreeNode(pre[preLeft]);
-        if (preLeft == preRight || postLeft == postRight) return root;   //说明区间只有一个节点，这个节点就是根节点,这里是一个关键的递归出口
+        if (preLeft == preRight || postLeft == postRight) return root;   //说明区间只有一个节点，这个节点就是根节点,这里是一个关键的递归出口，其实是因为find函数查找根节点时，至少要存在两个节点，否则会出现数组访问越界
         auto it = find(post.begin(), post.end(), pre[preLeft+1]);
         int i = distance(begin(post), it);
         root->left = buildTree(pre, preLeft+1, preLeft - postLeft + i + 1, post, postLeft, i);
