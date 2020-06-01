310650e83895ba735b42c6ca12a3383a38cb8936
JunpengCode <jpzhu.gm@gmail.com>
2020-06-01 10:52:16 +0800

    modify leetcode 204 solution

--- Traditional-Algorithms/LeetCode204.cpp
+++ Traditional-Algorithms/LeetCode204.cpp
@@ -18,3 +18,26 @@ public:
         return true;
     }
 };
+
+
+//推荐写法，上面写法在i 很大时，可能会溢出
+class Solution {
+public:
+    int countPrimes(int n) {
+        int count = 0;
+        for(int i = 2; i < n; i++){
+            if(isPrime(i)){
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    bool isPrime(int n){
+        if (n <= 1) return false;
+            for (int i = 2; i <= n/i; i++) {  //这里i <= n/i 不会有溢出问题
+                if (n % i == 0) return false;
+            }
+        return true;
+    }
+};
