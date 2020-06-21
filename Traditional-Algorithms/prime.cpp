957fb82a77541a309f8611463e6791711bac1441
JunpengCode <jpzhu.gm@gmail.com>
2020-06-21 18:46:11 +0800

    modify

--- Traditional-Algorithms/prime.cpp
+++ Traditional-Algorithms/prime.cpp
@@ -21,32 +21,29 @@ int main(){
     }
 }
 
-//筛法求质数，O(nlogn)
+
+//时间复杂度为O(nlogn)
 #include <iostream>
 using namespace std;
 
 const int N = 1e6+10;
+bool st[N];
 int prime[N], cnt;
-bool visited[N];
-int n;
 
-int factor(int n){
-    for(int i = 2; i <= n; i++){
-        if(!visited[i]){
-            prime[cnt++] = i;
-        }
-        for(int j = i + i; j <= n; j += i){
-            visited[j] = true;
-        }
+void isPrime(int x){
+    for(int i = 2; i <= x; i++){   //求出1-n中的质数，n可能为质数，所以要<=x
+        if(!st[i]) prime[cnt++] = i;
+        for(int j = i + i; j <= x; j += i) st[j] = true;
     }
 }
 
 int main(){
+    int n;
     scanf("%d", &n);
-    factor(n);
+    isPrime(n);
     printf("%d\n", cnt);
-    return 0;
 }
+
 
 //改进筛质数方法，O(nloglogn) 粗浅可以认为是O(n)
 #include <iostream>
