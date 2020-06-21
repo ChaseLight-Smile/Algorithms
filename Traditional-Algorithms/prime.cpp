70dfcbb88ff54fbc477a8919ae3e27db4bc44885
JunpengCode <jpzhu.gm@gmail.com>
2020-06-21 19:19:34 +0800

    modify

--- Traditional-Algorithms/prime.cpp
+++ Traditional-Algorithms/prime.cpp
@@ -45,7 +45,7 @@ int main(){
 }
 
 
-//改进筛质数方法，O(nloglogn) 粗浅可以认为是O(n)
+//改进筛质数方法，O(nloglogn) 粗浅可以认为是O(n)，每个合数用他的某个质因子(但不一定是最小质因子)筛掉即可
 #include <iostream>
 using namespace std;
 
@@ -74,3 +74,32 @@ int main(){
     printf("%d\n", cnt);
     return 0;
 }
+
+//线性筛选法，时间复杂度为O(n)，需要理解其证明过程。求出某个数的质因数的个数，并且在prime数组中给出了所有的这些质因数
+/**
+	线性筛选法的正确性证明：每个合数只会被其最小质因子筛掉，这是线性筛选的特性
+*/
+#include <iostream>
+using namespace std;
+const int N = 1e6+10;
+bool st[N];
+int prime[N], cnt;
+
+void factor(int n){
+    for(int i = 2; i <= n; i++){
+        if(!st[i]) prime[cnt++] = i;
+        for(int j = 0 ; prime[j] <= n/i; j++){  //枚举所有的质数
+            st[prime[j]*i] = true;
+            if(i % prime[j] == 0) break;    //说明prime[j]一定是i的最小质因子，因此循环退出
+        }
+    }
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    factor(n);
+    printf("%d\n", cnt);
+    return 0;
+}
+
