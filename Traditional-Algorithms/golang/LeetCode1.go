3fbec611f066c2b7f52693209435646516be655f
JunpengCode <jpzhu.gm@gmail.com>
2020-06-23 16:03:29 +0800

    modify leetcode 1 solution using golang Ac

--- Traditional-Algorithms/golang/LeetCode1.go
+++ Traditional-Algorithms/golang/LeetCode1.go
@@ -24,3 +24,14 @@ func twoSum(nums []int, target int) []int {
     }
     return nil
 }
+
+
+// 初始化map的方式不同
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)  // 和上一种方法初始化map的方式不同
+    for i := range nums {
+        if e, ok := m[target - nums[i]]; ok && i != m[target - nums[i]] {return []int{i, e}}
+        m[nums[i]] = i;
+    }
+    return nil
+}
