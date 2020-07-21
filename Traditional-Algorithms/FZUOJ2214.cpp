658a61977c5ded2aa3428e26c17aa87044fedf74
JunpengCode <jpzhu.gm@gmail.com>
2020-07-21 09:26:34 +0800

    add fzuoj 2214 solution AC

--- Traditional-Algorithms/FZUOJ2214.cpp
+++ Traditional-Algorithms/FZUOJ2214.cpp
@@ -27,3 +27,47 @@ int main(){
     
     return 0;
 }
+
+
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+typedef long long ll;
+const int N = 510, M = 1e4+10;
+const ll INF = 0x3f3f3f3f;
+int v[N];
+ll w[N];
+ll f[M];
+
+ll n, m;
+int main(){
+    int loop;
+    scanf("%d", &loop);
+    while(loop--){
+        memset(v, 0, sizeof v);
+        memset(w, 0, sizeof w);
+        memset(f, INF, sizeof f);
+        f[0] = 0;
+        scanf("%lld%lld", &n, &m);
+        ll sum = 0;
+        for(int i = 1; i <= n; i++) {
+            scanf("%lld%lld", &w[i], &v[i]);
+            sum += v[i];
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = sum; j >= v[i]; j--){
+                f[j] = min(f[j], f[j-v[i]]+w[i]);
+            }
+        }
+        for(ll i = sum; i >= 0; i--){
+            if(f[i] <= m){
+                printf("%lld\n", i);
+                break;
+            }
+        }
+    }
+    
+    return 0;
+}
