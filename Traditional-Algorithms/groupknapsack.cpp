4fdcddafdda3c0efffcf2b82e16897118de22536
JunpengCode <jpzhu.gm@gmail.com>
2020-07-24 10:19:11 +0800

    modify

--- Traditional-Algorithms/groupknapsack.cpp
+++ Traditional-Algorithms/groupknapsack.cpp
@@ -58,3 +58,30 @@ int main(){
     cout << f[m] << endl;
     return 0;
 }
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int N = 110;
+int n,m;
+int v[N], w[N];
+int f[N]; //表示小于等于j的物品的最大价值
+
+int main(){
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++){
+        int s;
+        cin >>s;
+        for(int j = 0; j < s; j++) cin >> v[j] >> w[j];
+        for(int j = m; j >= 0; j--){
+            for(int k = 0; k < s; k++){
+                if(j >= v[k]) f[j] = max(f[j], f[j-v[k]]+w[k]);
+            }
+        }
+    }
+    
+    cout << f[m] << endl;
+    return 0;
+}
