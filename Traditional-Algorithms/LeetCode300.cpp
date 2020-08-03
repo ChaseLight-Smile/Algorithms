dd15254dffef76e413ff428fa937d2d717cae2fe
JunpengCode <jpzhu.gm@gmail.com>
2020-08-03 18:22:36 +0800

    add leetcode 300 solution using greedy + binary search AC

--- Traditional-Algorithms/LeetCode300.cpp
+++ Traditional-Algorithms/LeetCode300.cpp
@@ -1,3 +1,4 @@
+//DP，时间复杂度为O(n^2)
 class Solution {
     static const int N = 1e4+10;
     int f[N]; // 表示当前以i为结尾的上升子序列长度
@@ -11,6 +12,27 @@ public:
         }
         int res = 0;
         for(int i = 0; i < nums.size(); i++) res = max(res, f[i]);
+        return res;
+    }
+};
+
+//贪心，时间复杂度为O(nlogn)
+class Solution {
+    static const int N = 1e4+10;
+    int f[N]; // 表示长度为i的子序列的结尾元素的最小值
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++){
+            int l = 0, r = res;
+            while(l < r){
+                int mid = l + r + 1 >> 1;
+                if(f[mid] < nums[i]) l = mid;
+                else r = mid-1;
+            }
+            res = max(res, r+1);
+            f[r+1] = nums[i];
+        }
         return res;
     }
 };
