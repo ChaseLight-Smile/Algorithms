732feb4036c0e74fa3bd1956fd14b6d63023b0ed
ChaseLight-Smile <jpzhu.gm@gmail.com>
2020-08-27 22:50:56 +0800

    add

--- LeetCodeConcurrencySolution/LeetCode1114.cpp
+++ LeetCodeConcurrencySolution/LeetCode1114.cpp
@@ -142,3 +142,37 @@ public:
         printThird();
     }
 };
+
+
+//lock guard + defer_lock
+class Foo {
+public:
+    mutex m1, m2, m3;
+public:
+    Foo() {
+        m2.lock();
+        m3.lock();
+    } //构造函数，当你构造类的变量的时候，就会默认先执行这个函数
+    //线程之间的同步，如果我不指定线程的启动顺序，那么整个执行过程就是乱序的，所以我要通过mutex来指定线程
+    //的启动顺序
+    void first(function<void()> printFirst) {
+        lock_guard<mutex> l(m1,std::defer_lock);
+        l.lock();
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        m2.unlock();
+    }
+
+    void second(function<void()> printSecond) {
+        lock_guard<mutex> l(m2);
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        m3.unlock();
+    }
+
+    void third(function<void()> printThird) {
+        lock_guard<mutex> l(m3);
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};
