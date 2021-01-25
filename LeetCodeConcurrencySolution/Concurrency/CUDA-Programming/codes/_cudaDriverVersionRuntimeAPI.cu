ee647c5ad8fae8879d94a9d57d034d0057ab13ca
EthanZhu-DB <jpzhu.gm@gmail.com>
2021-01-25 16:47:35 +0800

    add

--- LeetCodeConcurrencySolution/Concurrency/CUDA-Programming/codes/_cudaDriverVersionRuntimeAPI.cu
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cuda_runtime.h>
-
-int main(){
-  int driverVersion;
-  cudaDriverGetVersion(&driverVersion);
-  printf("CUDA Driver Version:%d.%d\n",driverVersion/1000,driverVersion/100);
-  return 0;
-}
--- LeetCodeConcurrencySolution/Concurrency/CUDA-Programming/codes/newton/newton.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// #include everything
-#include <newton/numeric_range.hpp>
-#include <newton/numeric_vector.hpp>
-#include <newton/math.hpp>
-
